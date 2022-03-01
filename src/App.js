4f8fe956a7efe8d2ca807cd3374f4f5142137ba7
loajanc <kshyun1432@gmail.com>
2022-03-01 16:50:37 +0900

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
 import { Entry } from "./pages/entry/Entry.page";
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>//Dashboard</DefaultLayout>
     </div>
   );
 }
