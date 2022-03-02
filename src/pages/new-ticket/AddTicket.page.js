ddb661d6e88efb110d7c2c8515dea13f9aaa41fa
loajanc <kshyun1432@gmail.com>
2022-03-02 17:30:44 +0900

    add new ticket

---
+++ src/pages/new-ticket/AddTicket.page.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AddTicketForm } from "../../components/add-ticket-form/AddTicketForm.comp";
+import { PageBreadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
+import { shortText } from "../../utils/validation";
+const initialFrmDt = {
+  subject: "",
+  issueDate: "",
+  detail: "",
+};
+
+const initialFrmError = {
+  subject: false,
+  issueDate: false,
+  detail: false,
+};
+
+export const AddTicket = () => {
+  const [frmData, setFrmData] = useState(initialFrmDt);
+  const [frmDataErro, setFrmDataErro] = useState(initialFrmDt);
+  useEffect(() => {}, [frmData, frmDataErro]);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFrmData({
+      ...frmData,
+      [name]: value,
+    });
+
+    console.log(name, value);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    setFrmDataErro(initialFrmError);
+
+    const isSubjectValid = await shortText(frmData.subject);
+
+    setFrmDataErro({
+      ...initialFrmError,
+      subject: !isSubjectValid,
+    });
+
+    console.log("Form submit request reveived", frmData);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <PageBreadcrumb page="New Ticket" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <AddTicketForm
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            frmDataErro={frmDataErro}
+            frmDt={frmData}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
